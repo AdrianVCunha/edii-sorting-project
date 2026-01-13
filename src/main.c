47e408a4341316bb76c7ec74573aa617d4db5a33
Adrian Vieira Cunha <adrianvieiracunha@gmail.com>
2026-01-13 20:25:02 -0300

    test: initial test with bubbleSort

--- src/main.c
+++ src/main.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#include "metrics/metric.h"
+#include "algorithms/bubble.h"
 
 int main(){
 	
-	printf("Hello, world!");
+	int arr[10];
+
+	for(int i = 0; i < 10; i++){
+		arr[i] = rand() % 100;
+	}
+
+	printf("Array before sorting:\n");
+
+	for(int i = 0; i < 10; i++){
+		printf("%d ", arr[i]);
+	}
+	printf("\n\n");
+
+	Metric m = createMetric();
+
+	clock_t start = clock(); 
+
+	bubbleSort(arr, 10, &m);
+
+	clock_t end = clock();
+	m.executionTime = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+	printf("Array after sorting:\n");
+	for(int i = 0; i < 10; i++){
+		printf("%d ", arr[i]);
+	}
+	printf("\n\nMetric after sorting:\n");
+	printMetric(m);
 
 	return 0;
 }
